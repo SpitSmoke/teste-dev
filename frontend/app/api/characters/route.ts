49136312326b54dc6730d00cc911071c0828f7ad
Jose Miguel <jmfelicio.sp@gmail.com>
2025-05-09 15:55:49 -0300

    final

--- frontend/app/api/characters/route.ts
+++ frontend/app/api/characters/route.ts
@@ -65,7 +65,7 @@ export async function GET(request: NextRequest) {
 
   try {
     // Usar o Axios para fazer a requisição à sua API backend
-    const response = await api.get('api/characters', {
+    const response = await api.get('/characters', {
       params: {
         page,
         name,
